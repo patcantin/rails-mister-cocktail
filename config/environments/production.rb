fe1980e6bd514b479152b0aea9c2137192fc77d3
patcantin <patcantin23@gmail.com>
2020-02-12 14:10:30 -0500

    devise/env/production rb config.action_mailer to keroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_mister_cocktail_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://dashboard.heroku.com/apps/mistercocktail7777' }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
